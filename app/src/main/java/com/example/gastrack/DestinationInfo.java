4379e71e68aa17a075982e4223d7f03feb8e2772
sz4321 <sebrinazeleke@Sebrinas-MacBook-Air.local>
2019-02-09 08:40:20 -0500

    Made the front page a litter bigger and added got gas image

--- app/src/main/java/com/example/gastrack/DestinationInfo.java
+++ app/src/main/java/com/example/gastrack/DestinationInfo.java
@@ -8,6 +8,10 @@ public class DestinationInfo extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_destination_info);
     }
 }
